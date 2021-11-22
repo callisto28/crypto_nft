3b15858b125117ee44e590e28113731a29ef351e
callisto28 <iotaseb@gmail.com>
2021-11-22 22:00:06 +0100

    DEL[convert]DEL tempo convert currencie

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import Image from "next/image";
 import Footer from "../components/Footer";
-import Convert from "./convert";
+// import Convert from "./convert";
 
 
 
@@ -20,18 +20,18 @@ export default function Home({ }) {
             Elles fonctionnent de manière autonome en dehors des systèmes bancaires et gouvernementaux traditionnels.</h2>
 
 
-          <div className="lg:col-start-1 lg:col-end-3 sm:col-span-6">
+          <div className="lg:col-start-1 lg:col-end-6 sm:col-span-6">
 
             <Image src="/images/main.jpg" alt="crypto" width={600} height={450} />
 
           </div>
-          <div className="lg:col-end-6 lg:col-span-3 sm:col-span-6 sm:text-center">
+          {/* <div className="lg:col-end-6 lg:col-span-3 sm:col-span-6 sm:text-center">
 
-            <p className="text-center text-l text-white">Possibilité de Convertir ci-dessous EUR/USD, EUR/BTC ...et bien plus</p>
-            <Convert />
+            <p className="text-center text-l text-white">Possibilité de Convertir ci-dessous EUR/USD, EUR/BTC ...et bien plus</p> */}
+          {/* <Convert /> */}
 
 
-          </div>
+          {/* </div> */}
 
           <p className="text-center text-l text-white col-start-1 col-end-7">
             Ici vous pouvez retrouver le top 15 des cryptomonnaies en temps réel au MarketCap, leurs cours et plus encore. Dans les infos ont retrouve une liste des 10 principaux exchanges qui sont présents sur le marché avec un lien vers leur site web. la possibilité de faire une recherche par nom de crypto (250 principal) est également disponible.
